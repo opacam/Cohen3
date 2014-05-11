coherence-project/Coherence

commit fdd1372e7ca64746b934b6486ec2dcd3d4d32098

Eliminate external log-module from flumotion, use the one from std-lib.
This modules is not available as package but was referenced using a
"svn:externals" property in the original Subversion repository. This
module may offer some features the std-lib does not. But I doubt it
is worth the effort of keeping a module with unkonw status.

coherence/log.py
@@ -1,97 +1,74 @@
 # Licensed under the MIT license
 # http://opensource.org/licenses/mit-license.php
 
-# Copyright 2007, Philippe Normand <philippe@fluendo.com>
+# Copyright 2013, Hartmut Goebel <h.goebel@crazy-compilers.com>
 
-from coherence.extern.log import log as externlog
-from coherence.extern.log.log import *
 import os
+import logging
+from logging import *
 
-def human2level(levelname):
-    levelname = levelname.lower()
-    if levelname.startswith('none'):
-        return 0
-    if levelname.startswith('error'):
-        return 1
-    if levelname.startswith('warn'):
-        return 2
-    if levelname.startswith('info'):
-        return 3
-    if levelname.startswith('debug'):
-        return 4
-    return 5
+LOG_FORMAT =('%(levelname)s %-27(category)s '
+             '%(asctime)s %(message)s '
+             '(%(filename)s:%(lineno))')
 
-def customStderrHandler(level, object, category, file, line, message):
+ENV_VAR_NAME = 'COHERENCE_DEBUG'
+
+
+class Loggable(logging.Logger):
     """
-    A log handler that writes to stderr.
+    Base class for objects that want to be able to log messages with
+    different level of severity.  The levels are, in order from least
+    to most: log, debug, info, warning, error.
 
-    @type level:    string
-    @type object:   string (or None)
-    @type category: string
-    @type message:  string
+    @cvar logCategory: Implementors can provide a category to log their
+       messages under.
     """
-    if not isinstance(message, basestring):
-        message = str(message)
 
-    if isinstance(message, unicode):
-        message = message.encode('utf-8')
+    logCategory = 'default'
 
-    message = "".join(message.splitlines())
-    where = "(%s:%d)" % (file, line)
+    def logObjectName(self):
+        """Overridable object name function."""
+        # cheat pychecker
+        for name in ['logName', 'name']:
+            if hasattr(self, name):
+                return getattr(self, name)
 
-    formatted_level = getFormattedLevelName(level)
-    formatted_time = time.strftime("%b %d %H:%M:%S")
-    formatted = '%s %-27s %-15s ' % (formatted_level, category,
-                                     formatted_time)
+        return None
 
-    safeprintf(sys.stderr, formatted)
-    safeprintf(sys.stderr, ' %s %s\n', message, where)
+    def log(self, message, *args, **kwargs):
+        logging.getLogger(self.logCategory).log(message, *args, **kwargs)
 
-    sys.stderr.flush()
+    def warning(self, message, *args, **kwargs):
+        logging.getLogger(self.logCategory).warning(message, *args, **kwargs)
 
-def init(logfile=None,loglevel='*:2'):
-    externlog.init('COHERENCE_DEBUG', True)
-    externlog.setPackageScrubList('coherence', 'twisted')
+    def info(self, message, *args, **kwargs):
+        logging.getLogger(self.logCategory).info(message, *args, **kwargs)
 
-    if logfile is not None:
-        outputToFiles(stdout=None, stderr=logfile)
+    def critical(self, message, *args, **kwargs):
+        logging.getLogger(self.logCategory).critical(message, *args, **kwargs)
 
-    # log WARNINGS by default
-    if not os.getenv('COHERENCE_DEBUG'):
-        if loglevel.lower() != 'none':
-            setDebug(loglevel)
+    def debug(self, message, *args, **kwargs):
+        logging.getLogger(self.logCategory).debug(message, *args, **kwargs)
 
-    if externlog.stderrHandler in externlog._log_handlers_limited:
-        externlog.removeLimitedLogHandler(externlog.stderrHandler)
-        if os.getenv('COHERENCE_DEBUG') or loglevel.lower() != 'none':
-            "print addLimitedLogHandler(customStderrHandler)"
-            externlog.addLimitedLogHandler(customStderrHandler)
+    def error(self, message, *args, **kwargs):
+        logging.getLogger(self.logCategory).error(message, *args, **kwargs)
 
-def set_debug(loglevel):
-    setDebug(loglevel)
+    def exception(self, message, *args, **kwargs):
+        logging.getLogger(self.logCategory).exception(message, *args, **kwargs)
 
-def show_levels():
-    print externlog._categories
+    fatal = critical
+    warn = warning
+    msg = info
 
-# Make Loggable a new-style object
-class Loggable(externlog.Loggable, object):
 
-    def logFunction(self, *args):
-        if len(args) > 1:
-            format = args[0]
-            arguments = args[1:]
-            try:
-                format % arguments
-            except TypeError:
-                format += " ".join(["%r" for i in arguments])
-            args = (format,) + arguments
-        return args
+def init(logfilename=None, loglevel=logging.WARN):
+    logger = logging.getLogger()
+    logging.addLevelName(100, 'NONE')
 
-    def critical(self, msg, *args):
-        self.warning(msg, *args)
+    logging.basicConfig(filename=logfilename, level=loglevel,
+                        format=LOG_FORMAT)
 
-    #def error(self, msg, *args):
-    #    self.log(msg, *args)
-
-    def msg(self, message, *args):
-        self.info(message, *args)
+    if ENV_VAR_NAME in os.environ:
+        logger.setLevel(os.environ[envVarName])
+    else:
+        logger.setLevel(loglevel)

coherence/base.py
@@ -258,7 +258,6 @@ class Coherence(log.Loggable):
             logmode = config.get('logging').get('level','warning')
         except (KeyError,AttributeError):
             logmode = config.get('logmode', 'warning')
-        _debug = []
 
         try:
             subsystems = config.get('logging')['subsystem']
@@ -271,23 +270,20 @@ class Coherence(log.Loggable):
                 except (KeyError,TypeError):
                     pass
                 self.info( "setting log-level for subsystem %s to %s" % (subsystem['name'],subsystem['level']))
-                _debug.append('%s:%d' % (subsystem['name'].lower(), log.human2level(subsystem['level'])))
+                logging.getLogger(subsystem['name'].lower()).setLevel(subsystem['level'].upper())
+
         except (KeyError,TypeError):
             subsystem_log = config.get('subsystem_log',{})
             for subsystem,level in subsystem_log.items():
                 #self.info( "setting log-level for subsystem %s to %s" % (subsystem,level))
-                _debug.append('%s:%d' % (subsystem.lower(), log.human2level(level)))
-        if len(_debug) > 0:
-            _debug = ','.join(_debug)
-        else:
-            _debug = '*:%d' % log.human2level(logmode)
+                logging.getLogger(subsystem.lower()).setLevel(level)
         try:
             logfile = config.get('logging').get('logfile',None)
             if logfile != None:
                 logfile = unicode(logfile)
         except (KeyError,AttributeError,TypeError):
             logfile = config.get('logfile', None)
-        log.init(logfile, _debug)
+        log.init(logfile, logmode.upper())
 
         self.warning("Coherence UPnP framework version %s starting..." % __version__)
 

opacam/Cohen3

commit 2f37d96abe5bd9f9a2ce04b0368abdac6793f8e0

Fix error in log-message-format introduced in fdd1372e7c.

coherence/log.py
@@ -9,7 +9,7 @@ import logging
 
 LOG_FORMAT = ('%(asctime)s %(levelname)s '
              '%(name)s: %(message)s '
-             '(%(filename)s:%(lineno))')
+             '(%(filename)s:%(lineno)s)')
 
 ENV_VAR_NAME = 'COHERENCE_DEBUG'
 