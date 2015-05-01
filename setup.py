d7f9ab3a241c5fa71e6b9590586d6c8e1cad3eb1
Unintended <unintended@users.noreply.github.com>
2015-05-01 21:25:16 +0300

    cleanup installation

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+
 __version__ = "0.7.0"
 
 try:
@@ -12,7 +13,7 @@ except:
     setuptools = None
     from distutils.core import setup
 
-    packages = ['coherence',]
+    packages = ['coherence', ]
 
     def find_packages(path):
         for f in os.listdir(path):
@@ -32,13 +33,13 @@ from distutils import log
 
 class build_docs(Command):
     description = "build documentation from rst-files"
-    user_options=[]
+    user_options = []
 
     def initialize_options(self):
       pass
 
     def finalize_options(self):
-        self.docpages = DOCPAGES
+      self.docpages = DOCPAGES
 
     def run(self):
         substitutions = ('.. |VERSION| replace:: '
@@ -76,14 +77,14 @@ except ImportError:
              "Using pre-build ones.")
 
 DOCPAGES = (
-    ('manpage', 'docs/man/coherence.rst', 'docs/man/coherence.1'),
-    )
+  ('manpage', 'docs/man/coherence.rst', 'docs/man/coherence.1'),
+)
 
 setup_args = {
-    'name': "Coherence",
-    'version': __version__,
-    'description': """Coherence - DLNA/UPnP framework for the digital living""",
-    'long_description': """
+  'name': "Coherence",
+  'version': __version__,
+  'description': """Coherence - DLNA/UPnP framework for the digital living""",
+  'long_description': """
 Coherence is a framework written in Python, providing a variety of
 UPnP MediaServer and UPnP MediaRenderer implementations for instant
 use.
@@ -133,87 +134,84 @@ Kudos go to:
 
 
 """ % __version__,
-    'author':"Frank Scholz",
-    'author_email':'dev@coherence-project.org',
-    'license' : "MIT",
-    'packages':packages,
-    'scripts' : ['bin/coherence','misc/Desktop-Applet/applet-coherence'],
-    'url' : "http://coherence-project.org",
-    'download_url' : 'http://coherence-project.org/download/Coherence-%s.tar.gz' % __version__,
-    'keywords':['UPnP', 'DLNA', 'multimedia', 'gstreamer'],
-    'classifiers' : ['Development Status :: 5 - Production/Stable',
-                   'Environment :: Console',
-                   'Environment :: Web Environment',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                ],
-    'package_data' : {
-        'coherence': ['upnp/core/xml-service-descriptions/*.xml',
-                      'ui/icons/*.png',
-                      'web/static/*.css','web/static/*.js'],
-        'misc': ['Desktop-Applet/*.png',
-                 'device-icons/*.png'],
-    },
+  'author': "Frank Scholz",
+  'author_email': 'dev@coherence-project.org',
+  'license': "MIT",
+  'packages': packages,
+  'scripts': ['bin/coherence'],
+  'url': "http://coherence-project.org",
+  'download_url': 'http://coherence-project.org/download/Coherence-%s.tar.gz' % __version__,
+  'keywords': ['UPnP', 'DLNA', 'multimedia', 'gstreamer'],
+  'classifiers': ['Development Status :: 5 - Production/Stable',
+                  'Environment :: Console',
+                  'Environment :: Web Environment',
+                  'License :: OSI Approved :: MIT License',
+                  'Operating System :: OS Independent',
+                  'Programming Language :: Python',
+                  ],
+  'package_data': {
+    'coherence': ['upnp/core/xml-service-descriptions/*.xml'],
+    'misc': ['device-icons/*.png'],
+  },
 }
 
 if setuptools:
-    setup_args['install_requires'] = [
-        'ConfigObj >= 4.3',
-        'Twisted >= 14.0',
-        'zope.interface',
-        'louie',
-        'livestreamer',
-        'lxml',
-        'python-dateutil',
-        'pyopenssl'
-    ]
-    if sys.platform in ('win32', 'sunos5'):
-        setup_args['install_requires'].append('Netifaces >= 0.4')
+  setup_args['install_requires'] = [
+    'ConfigObj >= 4.3',
+    'Twisted >= 14.0',
+    'zope.interface',
+    'louie',
+    'livestreamer',
+    'lxml',
+    'python-dateutil',
+    'pyopenssl'
+  ]
+  if sys.platform in ('win32', 'sunos5'):
+    setup_args['install_requires'].append('Netifaces >= 0.4')
 
-    setup_args['entry_points'] = """
-        [coherence.plugins.backend.media_server]
-        FSStore = coherence.backends.fs_storage:FSStore
-        MediaStore = coherence.backends.mediadb_storage:MediaStore
-        ElisaMediaStore = coherence.backends.elisa_storage:ElisaMediaStore
-        FlickrStore = coherence.backends.flickr_storage:FlickrStore
-        AxisCamStore = coherence.backends.axiscam_storage:AxisCamStore
-        BuzztardStore = coherence.backends.buzztard_control:BuzztardStore
-        IRadioStore = coherence.backends.iradio_storage:IRadioStore
-        LastFMStore = coherence.backends.lastfm_storage:LastFMStore
-        AmpacheStore = coherence.backends.ampache_storage:AmpacheStore
-        TrackerStore = coherence.backends.tracker_storage:TrackerStore
-        DVBDStore = coherence.backends.dvbd_storage:DVBDStore
-        AppleTrailersStore = coherence.backends.appletrailers_storage:AppleTrailersStore
-        LolcatsStore = coherence.backends.lolcats_storage:LolcatsStore
-        TEDStore = coherence.backends.ted_storage:TEDStore
-        BBCStore = coherence.backends.bbc_storage:BBCStore
-        SWR3Store = coherence.backends.swr3_storage:SWR3Store
-        Gallery2Store = coherence.backends.gallery2_storage:Gallery2Store
-        YouTubeStore = coherence.backends.youtube_storage:YouTubeStore
-        MiroGuideStore = coherence.backends.miroguide_storage:MiroGuideStore
-        ITVStore = coherence.backends.itv_storage:ITVStore
-        PicasaStore = coherence.backends.picasa_storage:PicasaStore
-        TestStore = coherence.backends.test_storage:TestStore
-        PlaylistStore = coherence.backends.playlist_storage:PlaylistStore
-        YamjStore = coherence.backends.yamj_storage:YamjStore
-        BansheeStore = coherence.backends.banshee_storage:BansheeStore
-        FeedStore = coherence.backends.feed_storage:FeedStore
-        RadiotimeStore = coherence.backends.radiotime_storage:RadiotimeStore
-        AudioCDStore = coherence.backends.audiocd_storage:AudioCDStore
-        TwitchStore = coherence.backends.twitch_storage:TwitchStore
-        
-        [coherence.plugins.backend.media_renderer]
-        ElisaPlayer = coherence.backends.elisa_renderer:ElisaPlayer
-        GStreamerPlayer = coherence.backends.gstreamer_renderer:GStreamerPlayer
-        BuzztardPlayer = coherence.backends.buzztard_control:BuzztardPlayer
+  setup_args['entry_points'] = """
+    [coherence.plugins.backend.media_server]
+    FSStore = coherence.backends.fs_storage:FSStore
+    MediaStore = coherence.backends.mediadb_storage:MediaStore
+    ElisaMediaStore = coherence.backends.elisa_storage:ElisaMediaStore
+    FlickrStore = coherence.backends.flickr_storage:FlickrStore
+    AxisCamStore = coherence.backends.axiscam_storage:AxisCamStore
+    BuzztardStore = coherence.backends.buzztard_control:BuzztardStore
+    IRadioStore = coherence.backends.iradio_storage:IRadioStore
+    LastFMStore = coherence.backends.lastfm_storage:LastFMStore
+    AmpacheStore = coherence.backends.ampache_storage:AmpacheStore
+    TrackerStore = coherence.backends.tracker_storage:TrackerStore
+    DVBDStore = coherence.backends.dvbd_storage:DVBDStore
+    AppleTrailersStore = coherence.backends.appletrailers_storage:AppleTrailersStore
+    LolcatsStore = coherence.backends.lolcats_storage:LolcatsStore
+    TEDStore = coherence.backends.ted_storage:TEDStore
+    BBCStore = coherence.backends.bbc_storage:BBCStore
+    SWR3Store = coherence.backends.swr3_storage:SWR3Store
+    Gallery2Store = coherence.backends.gallery2_storage:Gallery2Store
+    YouTubeStore = coherence.backends.youtube_storage:YouTubeStore
+    MiroGuideStore = coherence.backends.miroguide_storage:MiroGuideStore
+    ITVStore = coherence.backends.itv_storage:ITVStore
+    PicasaStore = coherence.backends.picasa_storage:PicasaStore
+    TestStore = coherence.backends.test_storage:TestStore
+    PlaylistStore = coherence.backends.playlist_storage:PlaylistStore
+    YamjStore = coherence.backends.yamj_storage:YamjStore
+    BansheeStore = coherence.backends.banshee_storage:BansheeStore
+    FeedStore = coherence.backends.feed_storage:FeedStore
+    RadiotimeStore = coherence.backends.radiotime_storage:RadiotimeStore
+    AudioCDStore = coherence.backends.audiocd_storage:AudioCDStore
+    TwitchStore = coherence.backends.twitch_storage:TwitchStore
 
-        [coherence.plugins.backend.binary_light]
-        SimpleLight = coherence.backends.light:SimpleLight
+    [coherence.plugins.backend.media_renderer]
+    ElisaPlayer = coherence.backends.elisa_renderer:ElisaPlayer
+    GStreamerPlayer = coherence.backends.gstreamer_renderer:GStreamerPlayer
+    BuzztardPlayer = coherence.backends.buzztard_control:BuzztardPlayer
 
-        [coherence.plugins.backend.dimmable_light]
-        BetterLight = coherence.backends.light:BetterLight
-    """
+    [coherence.plugins.backend.binary_light]
+    SimpleLight = coherence.backends.light:SimpleLight
+
+    [coherence.plugins.backend.dimmable_light]
+    BetterLight = coherence.backends.light:BetterLight
+  """
 
 
 setup(cmdclass=cmdclass, **setup_args)
