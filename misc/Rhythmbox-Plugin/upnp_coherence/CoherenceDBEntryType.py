43efb80945e056eee00e0e7c7ae301bcfbcd0302
Unintended <unintended@users.noreply.github.com>
2015-05-01 21:25:16 +0300

    cleanup installation

--- misc/Rhythmbox-Plugin/upnp_coherence/CoherenceDBEntryType.py
+++
@@ -1,7 +0,0 @@
-import rhythmdb
-
-
-class CoherenceDBEntryType(rhythmdb.EntryType):
-    def __init__(self, client_id):
-        entry_name = "CoherenceUpnp:%s", client_id
-        rhythmdb.EntryType.__init__(self, name=entry_name)
