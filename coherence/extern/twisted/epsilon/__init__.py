71b6a1762de4810a150721c24e5c3f97827c4847
Pol Canelles <pol@canellestudi.com>
2018-09-03 18:44:06 +0200

    Fix external.twisted import
    Fails while importing the recently added external dependencies twisted.axiom. Removes unneeded prints.

--- coherence/extern/twisted/epsilon/__init__.py
+++ coherence/extern/twisted/epsilon/__init__.py
@@ -7,7 +7,7 @@ from twisted.python import versions
 
 
 def asTwistedVersion(packageName, versionString):
-    print(*list(map(int, versionString.split('+', 1)[0].split("."))))
+    # print(*list(map(int, versionString.split('+', 1)[0].split("."))))
     return versions.Version(
         packageName, *list(map(int, versionString.split('+', 1)[0].split("."))))
 
