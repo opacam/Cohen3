23856acd65cf65c822c573dfb3e19272b38232c6
opacam <canellestudi@gmail.com>
2018-10-15 11:18:40 +0200

    Refactor backend: TEDStore
    The recently introduced module coherence.backends.models allow us to refactor some backends in order to be more maintainable. Also add module documentation.
    
    References: 451b5e0, 74ab3a9, fb620d0

--- coherence/backends/ted_storage.py
+++ coherence/backends/ted_storage.py
@@ -4,87 +4,101 @@
 # http://opensource.org/licenses/mit-license.php
 
 # Copyright 2008, Benjamin Kampmann <ben.kampmann@googlemail.com>
+# Copyright 2018, Pol Canelles <canellestudi@gmail.com>
 
 """
-Another simple rss based Media Server, this time for TED.com content
+TEDStore
+--------
+
+Another simple rss based Media Server, this time for TED.com content.
+
+Example to run from python script::
+
+    from coherence.base import Coherence
+    from twisted.internet import reactor
+
+    coherence = Coherence(
+        {'logmode': 'info',
+         'plugin': {'backend': 'TEDStore',
+                    'name': 'Cohen3 TEDStore'
+                    },
+         }
+    )
+    reactor.run()
+
+Example to run from console::
+
+    cohen3 --plugin=backend:TEDStore
+
+.. note:: you need the cohen 3 package installed to run the plugin from
+          a console.
+
+.. versionchanged:: 0.8.3
 """
 
-from coherence.backend import BackendItem
-from coherence.backends.appletrailers_storage import Container
-# I can reuse stuff. cool. But that also means
-# we might want to refactor it into a base class to reuse
-from coherence.backends.lolcats_storage import LolcatsStore
-from coherence.upnp.core import DIDLLite
+from coherence.backends.models.items import BackendVideoItem
+from coherence.backends.models.stores import BackendVideoStore
 
 
-class TedTalk(BackendItem):
+class TedTalk(BackendVideoItem):
+    '''
+    The Backend Item.
 
-    def __init__(self, parent_id, id, title=None, url=None,
-                 duration=None, size=None):
-        BackendItem.__init__(self)
-        self.parentid = parent_id
-        self.update_id = 0
-        self.id = id
-        self.location = url
-        self.name = title
+    .. versionchanged:: 0.8.3
+       Refactored using the class
+       :class:`~coherence.backends.models.items.BackendVideoItem`
+    '''
+    mimetype = 'video/mp4'
 
-        self.item = DIDLLite.VideoItem(id, parent_id, self.name)
+    def __init__(self, parent_id, item_id, urlbase, **kwargs):
+        super(TedTalk, self).__init__(
+            parent_id, item_id, urlbase, **kwargs)
 
-        res = DIDLLite.Resource(
-            self.location,
-            'http-get:*:video/mp4:*')  # FIXME should be video/x-m4a
-        res.size = size
-        res.duration = duration
-        self.item.res.append(res)
+        self.item.res.size = kwargs.get('size', None)
+        self.item.res.duration = kwargs.get('duration', None)
 
 
-class TEDStore(LolcatsStore):
+class TEDStore(BackendVideoStore):
+    '''
+    The Backend Store.
+
+    .. versionchanged:: 0.8.3
+       Refactored using the class
+       :class:`~coherence.backends.models.stores.BackendVideoStore`
+    '''
+    logCategory = 'ted_store'
     implements = ['MediaServer']
 
-    rss_url = "http://feeds.feedburner.com/tedtalks_video?format=xml"
+    name = 'TEDTalks'
 
-    ROOT_ID = 0
+    upnp_protocols = [
+        'http-get:*:video/quicktime:*',
+        'http-get:*:video/mp4:*',
+    ]
+
+    root_url = b"http://feeds.feedburner.com/tedtalks_video?format=xml"
+    root_find_items = './channel/item'
+    root_id = 0
+
+    item_cls = TedTalk
+    item_type = 'http-get:*:video/mp4:*'
 
     def __init__(self, server, *args, **kwargs):
-        LolcatsStore.__init__(self, server, **kwargs)
+        super(TEDStore, self).__init__(server, **kwargs)
 
-        self.name = kwargs.get('name', 'TEDtalks')
         self.refresh = int(kwargs.get('refresh', 1)) * (60 * 60)
 
-        self.next_id = 1001
         self.last_updated = None
 
-        self.container = Container(None, self.ROOT_ID, self.name)
-
-        self.videos = {}
-
-        dfr = self.update_data()
-        dfr.addCallback(self.init_completed)
-
-    def get_by_id(self, id):
-        if int(id) == self.ROOT_ID:
-            return self.container
-        return self.videos.get(int(id), None)
-
-    def upnp_init(self):
-        if self.server:
-            self.server.connection_manager_server.set_variable(
-                0, 'SourceProtocolInfo', ['http-get:*:video/mp4:*'])
-
-    def parse_data(self, xml_data):
-
-        root = xml_data.getroot()
-
+    def parse_data(self, root):
         pub_date = root.find('./channel/lastBuildDate').text
-
         if pub_date == self.last_updated:
             return
 
         self.last_updated = pub_date
+        return super(TEDStore, self).parse_data(root)
 
-        self.container.children = []
-        self.videos = {}
-
+    def parse_item(self, item):
         # FIXME: move these to generic constants somewhere
         mrss = './{http://search.yahoo.com/mrss/}'
         itunes = './{http://www.itunes.com/dtds/podcast-1.0.dtd}'
@@ -93,36 +107,18 @@ class TEDStore(LolcatsStore):
         duration = itunes + 'duration'
         summary = itunes + 'summary'
 
-        for item in root.findall('./channel/item'):
-            data = {}
-            data['parent_id'] = self.ROOT_ID
-            data['id'] = self.next_id
-            data['title'] = item.find(
-                './title').text.replace('TEDTalks : ', '')
-            # data ['summary'] = item.find(summary).text
-            # data ['duration'] = item.find(duration).text
+        data = {
+            'name': item.find(
+                './title').text.replace('TEDTalks : ', ''),
+            'summary': item.find(summary).text,
+            'duration': item.find(duration).text
+        }
 
-            try:
-                media_entry = item.find(url_item)
-                data['url'] = media_entry.get('url', None)
-                data['size'] = media_entry.get('size', None)
-            except IndexError:
-                continue
-
-            video = TedTalk(**data)
-
-            self.container.children.append(video)
-            self.videos[self.next_id] = video
-
-            self.next_id += 1
-
-        self.container.update_id += 1
-        self.update_id += 1
-
-        if self.server and hasattr(self.server, 'content_directory_server'):
-            # the content_directory_server may not yet be initialised
-            self.server.content_directory_server.set_variable(
-                0, 'SystemUpdateID', self.update_id)
-            value = (self.ROOT_ID, self.container.update_id)
-            self.server.content_directory_server.set_variable(
-                0, 'ContainerUpdateIDs', value)
+        try:
+            media_entry = item.find(url_item)
+            data['url'] = media_entry.get('url', None)
+            data['size'] = media_entry.get('fileSize', None)
+            data['mimetype'] = media_entry.get('type', None)
+        except IndexError:
+            return None
+        return data
